77642d46286d39a7c417413e438591b519d186e2
Prem Jishnu Ganapathy <premjishnu.g@gmail.com>
2026-09-20 13:05:33 -0400

    Initial Portfolio Website

---
+++ script.js
@@ -0,0 +1,22 @@
+const elements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+
+        entries.forEach((entry) => {
+
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+
+        });
+
+    },
+    {
+        threshold: 0.1
+    }
+);
+
+elements.forEach((element) => {
+    observer.observe(element);
+});
